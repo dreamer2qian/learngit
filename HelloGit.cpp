f988b5ac29e43e73de138bdc8e3477a0febd23d7
dreamer2qian <1198232488@qq.com>
2019-10-06 13:45:18 +0800

    add HelloGit.cpp

---
+++ HelloGit.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+
+	printf("Begin to learn how to use git.\n");
+
+	printf("Git is free software.\n");
+
+	printf("Millions of people are using git to manage the repository.\n");
+
+	printf("You really need to master git to enhance your effiency\n");
+
+
+
+	
+
+	return 0;
+}
